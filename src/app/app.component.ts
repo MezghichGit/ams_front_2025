1e35bcc1b26732f2c332327ca6827edc7b260e20
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-02-16 16:45:19 +0100

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SIP ACADEMY';
+  logo:string="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMcOuz17dyeXVHeUdUgaSRcvgDb7hO06tGjw&s";
 
   formation:string = "Fullstack";
 }
