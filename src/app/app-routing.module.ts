ae7c2f60502321e5b32bf2270e5e6b1621fbf601
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-03-02 13:35:30 +0100

    Guard sur les routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,18 +8,19 @@ import { AddArticleComponent } from './add-article/add-article.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
-{ path: "listProvider", component: ListProvidersComponent },
-{ path: "addProvider", component: AddProviderComponent},
-{ path: "updateProvider/:id", component: UpdateProviderComponent},
-{ path: "listArticles", component: ListArticlesComponent},
-{ path: "addArticle", component: AddArticleComponent},
+{ path: "listProvider", component: ListProvidersComponent, canActivate:[AuthGuardService] },
+{ path: "addProvider", component: AddProviderComponent, canActivate:[AuthGuardService]},
+{ path: "updateProvider/:id", component: UpdateProviderComponent, canActivate:[AuthGuardService]},
+{ path: "listArticles", component: ListArticlesComponent, canActivate:[AuthGuardService]},
+{ path: "addArticle", component: AddArticleComponent, canActivate:[AuthGuardService]},
 { path: "signup", component: SignupComponent},
 { path: "login", component: LoginComponent},
-{ path: "logout", component: LogoutComponent},
-{ path: 'sav', loadChildren: () => import('./sav/sav.module').then(m => m.SavModule) },
-{ path: 'registration', loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule) }
+{ path: "logout", component: LogoutComponent, canActivate:[AuthGuardService]},
+{ path: 'sav', loadChildren: () => import('./sav/sav.module').then(m => m.SavModule) , canActivate:[AuthGuardService]},
+{ path: 'registration', loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule) , canActivate:[AuthGuardService]}
 ];
 
 @NgModule({
